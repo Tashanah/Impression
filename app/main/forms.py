54b7e6bfb466a850ff75e8636256156b97f67496
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-22 14:15:54 +0300

    review and update profile form added

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class ReviewForm(FlaskForm):
+    
+    title  = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Movie Review', validators=[Required()])
+    submit = SubmitField('submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators = [Required ()])
+    submit = SubmitField('Submit')
+
+    
+    
+    
