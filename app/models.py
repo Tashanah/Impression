ad09d606fdab288fcc708ca1d3756239ae82d6ec
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-22 13:20:52 +0300

    login class created

--- app/models.py
+++ app/models.py
@@ -44,7 +44,43 @@ class review:
         for review in cls.all_reviews:
             if review.pitch ==pitch:
                 response.append(review)
+
         return response
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True, index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+        
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ ='roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role' ,lazy= "dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 
 
