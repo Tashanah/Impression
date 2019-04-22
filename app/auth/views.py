3fb9aeb5cb476fb568484b13e8a0a9feb5d13a11
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-22 14:14:26 +0300

    error pages created

---
+++ app/auth/views.py
@@ -0,0 +1,41 @@
+from . import auth 
+from flask import render_template,redirect,url_for,flash,request,abort
+from flask_login import login_user
+from ..models import Review, User
+from .forms import RegistrationForm
+from .. import db
+
+@auth.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Impressions login"
+    return render_template('auth/login.html',login_form = login_form,title=title) 
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)  
+    
