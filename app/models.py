93c8a20eb160b69628c4625411f421a589dbf737
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-04-24 11:37:00 +0300

    flask imported to email.py

--- app/models.py
+++ app/models.py
@@ -70,7 +70,7 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-    pitches=db.relationship('Pitch', backref='user', lazy="dynamic")
+    # pitches=db.relationship('Pitch', backref='user', lazy="dynamic")
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
